fd53b6e93193dbfd59be88963c52587791a9adbd
ndoelger <doelgern@gmail.com>
2023-06-02 10:37:57 -0400

    Push1

---
+++ app.js
@@ -0,0 +1,93 @@
+$(() => {});
+
+const prompt = require("prompt-sync")();
+
+// Array of words to guess from
+const words = ["javascript", "hangman", "game", "programming", "openai"];
+
+// Choosing the words from the array
+let word = words[Math.floor(Math.random() * words.length)];
+
+//This is an array that will be used to display the letters that you have correct
+let wordArray = word.split("");
+
+//This will end the game when you guess the word
+let correctLetters = [];
+
+// Create an array to store the guessed letters
+let guessedLetters = [];
+
+// Variable to track the number of attempts
+let attempts = 10;
+
+let stillPlaying = true;
+
+// Function to start the game
+const username = prompt("What is your name? ");
+console.log(`Your name is ${username}`);
+
+function startGame() {
+  word = words[Math.floor(Math.random() * words.length)];
+
+  wordArray = word.split("");
+
+  console.log(wordArray);
+
+  correctLetters = [];
+
+  guessedLetters = [];
+
+  attempts = 10;
+
+  while (attempts > 0 && stillPlaying) {
+    console.log("**********");
+    console.log(`Attempts: ${attempts}`);
+    guess = prompt("Please guess a letter: ");
+    guessLetter(guess);
+  }
+}
+startGame();
+
+if (!attempts) console.log(`Game over! The correct word was ${word}.`);
+
+// Function to display the current state of the word
+function displayWord() {
+  let display = "";
+  for (let char of wordArray) {
+    if (correctLetters.includes(char)) display += char;
+    else {
+      display += "_";
+    }
+  }
+  console.log(display);
+  console.log(guessedLetters.sort());
+
+  if (!display.includes("_")) {
+    let playAgain = prompt("You win! Play again? (Y/N)").toLowerCase();
+    if (playAgain === "y") startGame();
+    else stillPlaying = false;
+  }
+}
+
+// Function to process the user's guess
+function guessLetter(letter) {
+  if (
+    guessedLetters.includes(letter) ||
+    guess.length !== 1 ||
+    typeof letter !== "string" ||
+    false
+  ) {
+    console.log("Invalid answer");
+    return;
+  }
+  if (word.includes(letter)) {
+    console.log("Correct guess!");
+    guessedLetters.push(letter);
+    correctLetters.push(letter);
+  } else {
+    attempts--;
+    console.log(`Nope :( Try again.`);
+    guessedLetters.push(letter);
+  }
+  displayWord();
+}
